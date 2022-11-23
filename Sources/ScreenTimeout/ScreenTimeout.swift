74fc07a56d6705b1940d21a164b78d076fba170c
SNQ-2001 <taishin05109@gmail.com>
2022-11-23 21:50:54 +0900

    add: source code

--- Sources/ScreenTimeout/ScreenTimeout.swift
+++ Sources/ScreenTimeout/ScreenTimeout.swift
@@ -1,6 +1,54 @@
-public struct ScreenTimeout {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public extension View {
+    func onTimeout(seconds: TimeInterval = 30, perform: ((ScreenStates) -> Void)?) -> some View {
+        Timeout(content: self, seconds: seconds) { status in
+            if let perform {
+                perform(status)
+            }
+        }
+    }
+}
+
+public enum ScreenStates {
+    case active
+    case timeout
+}
+
+fileprivate struct Timeout<T: View>: View {
+    var content: T
+    var seconds: TimeInterval
+    var perform: ((ScreenStates) -> Void)
+
+    @State var timer: Timer? = nil
+    @State var states: ScreenStates = .timeout
+
+    var body: some View {
+        content
+            .onTapGesture {
+                start()
+            }
+            .onAppear {
+                start()
+            }
+            .onDisappear {
+                stop()
+            }
+    }
+
+    private func start() {
+        if states == .timeout {
+            states = .active
+            perform(.active)
+        }
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: seconds, repeats: false) { _ in
+            states = .timeout
+            perform(.timeout)
+        }
+    }
+
+    private func stop() {
+        timer?.invalidate()
     }
 }
